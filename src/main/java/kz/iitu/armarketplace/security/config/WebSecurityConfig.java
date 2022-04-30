ed71f0da82c28562ed7fa1a5d384178b13d34e13
adal <aldongaradal@gmail.com>
2022-04-30 20:14:18 +0600

    fix: temp file security

--- src/main/java/kz/iitu/armarketplace/security/config/WebSecurityConfig.java
+++ src/main/java/kz/iitu/armarketplace/security/config/WebSecurityConfig.java
@@ -65,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.antMatchers("/api/products/**").permitAll()
+			.antMatchers("/api/files/**").permitAll()
 			.antMatchers("/**").permitAll() //todo temp
 			.anyRequest().authenticated();
 
